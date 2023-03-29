a26744b77ee18f7586be25c3168ac0b98edea53b
vophuc0271 <emtai789@gmail.com>
2023-03-29 22:57:05 +0700

    update

--- laravel/database/migrations/2019_12_14_000001_create_personal_access_tokens_table.php
+++ laravel/database/migrations/2019_12_14_000001_create_personal_access_tokens_table.php
@@ -13,8 +13,8 @@ return new class extends Migration
     {
         Schema::create('personal_access_tokens', function (Blueprint $table) {
             $table->id();
-            $table->morphs('tokenable');
-            $table->string('name');
+            //$table->morphs('tokenable');
+            $table->string('name',100);
             $table->string('token', 64)->unique();
             $table->text('abilities')->nullable();
             $table->timestamp('last_used_at')->nullable();
